a8676243a81f269398293f7ce5dfcda3e483dc5c
Christopher L. Lübbers <wsad@posteo.net>
2026-01-20 11:43:35 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,21 +9,25 @@ def load_data(file_path):
 
 animals_data = load_data("animals_data.json")
 
+output = ""
 for animal in animals_data:
-    output = []
-
     if "name" in animal:
-        output.append(f"Name: {animal['name']}")
-
-    if "characteristics" in animal:
-        if "diet" in animal["characteristics"]:
-            output.append(f"Diet: {animal['characteristics']['diet']}")
-        if "type" in animal["characteristics"]:
-            output.append(f"Type: {animal['characteristics']['type']}")
-
+        output += f"Name: {animal['name']}\n"
+    if "diet" in animal["characteristics"]:
+        output += f"Diet: {animal['characteristics']['diet']}\n"
     if "locations" in animal:
-        locations_str = ", ".join(animal["locations"])
-        output.append(f"Location: {locations_str}")
+        output += f"Location: {', '.join(animal['locations'])}\n\n"
+    if "type" in animal["characteristics"]:
+        output += f"Type: {animal['characteristics']['type']}\n"
 
-    if output:
-        print("\n".join(output) + "\n")
+print(output)
+
+with open("animals_template.html", "r") as template_file:
+    template = template_file.read()
+
+template = template.replace("__REPLACE_ANIMALS_INFO__", output)
+
+print(template)
+
+with open("animals.html", "w") as output_file:
+    output_file.write(template)
